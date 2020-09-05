f6cb810888c9f9ee2ba4ed7a6b3c93629c3cf39d
Janne Hellsten <hellsten@live.com>
2020-09-05 18:18:31 +0300

    feat: added button for delete book, added endpoint to delete book

--- src/main/java/com/hellsten/bookstore/training/web/BookController.java
+++ src/main/java/com/hellsten/bookstore/training/web/BookController.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BookController {
@@ -25,4 +28,9 @@ public class BookController {
         }
         return "book";
     }
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+    repository.deleteById(bookId);
+    return "redirect:../booklist";
+    }   
 }
