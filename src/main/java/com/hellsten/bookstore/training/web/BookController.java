2a70059cb0310f5673ee60d74e026467b365732b
Janne Hellsten <hellsten@live.com>
2020-09-05 21:05:04 +0300

    feat added new book and edit book endpoints

--- src/main/java/com/hellsten/bookstore/training/web/BookController.java
+++ src/main/java/com/hellsten/bookstore/training/web/BookController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -33,4 +34,24 @@ public class BookController {
     repository.deleteById(bookId);
     return "redirect:../booklist";
     }   
+
+    @RequestMapping(value = "/newbook", method = RequestMethod.GET)
+    public String newBook(Model model) {
+    model.addAttribute("book", new Book());
+    return "addbook";
+    }   
+
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public String editBook(@PathVariable("id") Long bookId, Model model) {
+    model.addAttribute("book", repository.findById(bookId));
+    return "editbook";
+    }   
+
+    @RequestMapping(value = "/newbook", method = RequestMethod.POST)
+    public String saveBook(@ModelAttribute Book book, Model model) {
+    System.out.println(book);
+        repository.save(book);
+    return "redirect:booklist";
+    }   
+ 
 }
