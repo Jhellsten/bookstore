03dc674faba405dd2cd8b1ab93cd710d3815ca71
Janne Hellsten <hellsten@live.com>
2020-09-05 11:34:56 +0300

    feat: added H2 database and url for it

--- src/main/java/com/hellsten/bookstore/training/domain/Book.java
+++ src/main/java/com/hellsten/bookstore/training/domain/Book.java
@@ -1,6 +1,17 @@
 package com.hellsten.bookstore.training.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    
     private String title;
     private String author;
     private int year;
