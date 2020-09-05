4a35a766e2ca2a6f5122556533ab2f63e4ea2a95
Janne Hellsten <hellsten@live.com>
2020-09-05 17:27:44 +0300

    feat: added database seeding for testing

--- src/main/java/com/hellsten/bookstore/training/TrainingApplication.java
+++ src/main/java/com/hellsten/bookstore/training/TrainingApplication.java
@@ -1,13 +1,29 @@
 package com.hellsten.bookstore.training;
 
+import com.hellsten.bookstore.training.domain.Book;
+import com.hellsten.bookstore.training.domain.BookRepo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TrainingApplication {
+public class TrainingApplication implements CommandLineRunner {
 
+	@Autowired
+	private BookRepo repository;
 	public static void main(String[] args) {
 		SpringApplication.run(TrainingApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) {
+		repository.deleteAll();
+
+        repository.save(new Book("A Farewell to Arms", "Ernest Hemingway", 1929,"122323233232-2",  10.00));
+        repository.save(new Book("Animal Farm", "George Orwell", 1945, "22123322-3", 100.00));
+	}
+
 
 }
