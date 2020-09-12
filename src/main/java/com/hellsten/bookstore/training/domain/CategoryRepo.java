9e9d13026951029eb0c75e39594fec957e2ff3b6
Janne Hellsten <hellsten@live.com>
2020-09-12 09:40:04 +0300

    feat: category entity and category repository

---
+++ src/main/java/com/hellsten/bookstore/training/domain/CategoryRepo.java
@@ -0,0 +1,11 @@
+package com.hellsten.bookstore.training.domain;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepo extends CrudRepository<Category, Long>  {
+
+    List<Category> findByName(String name);
+
+    
+}
