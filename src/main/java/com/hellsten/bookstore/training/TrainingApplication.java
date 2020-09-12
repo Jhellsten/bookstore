8cb31373e540c51331df618f966b9f2b03a67ebb
Janne Hellsten <hellsten@live.com>
2020-09-12 10:12:07 +0300

    chores: updated category names

--- src/main/java/com/hellsten/bookstore/training/TrainingApplication.java
+++ src/main/java/com/hellsten/bookstore/training/TrainingApplication.java
@@ -5,7 +5,6 @@ import com.hellsten.bookstore.training.domain.BookRepo;
 import com.hellsten.bookstore.training.domain.Category;
 import com.hellsten.bookstore.training.domain.CategoryRepo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -22,12 +21,12 @@ public class TrainingApplication {
 	public CommandLineRunner booksDemo(BookRepo books, CategoryRepo categories) {
 		return (args) -> {
 			System.out.println("save a couple of books");
-			categories.save(new Category("IT"));
-			categories.save(new Category("Business"));
-			categories.save(new Category("Law"));
+			categories.save(new Category("Fiction"));
+			categories.save(new Category("Fantasy"));
+			categories.save(new Category("History"));
 
-			books.save(new Book("A Farewell to Arms", "Ernest Hemingway", 1929,"122323233232-2", categories.findByName("IT").get(0)));
-        	books.save(new Book("Animal Farm", "George Orwell", 1945, "22123322-3", categories.findByName("Business").get(0)));
+			books.save(new Book("A Farewell to Arms", "Ernest Hemingway", 1929,"122323233232-2", categories.findByName("Fiction").get(0)));
+        	books.save(new Book("Animal Farm", "George Orwell", 1945, "22123322-3", categories.findByName("Fantasy").get(0)));
 			
 			System.out.println("fetch all Books");
 			for (Book book : books.findAll()) {
